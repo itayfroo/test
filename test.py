979ee668a09b9f25e58faa919f35ed6163c7a549
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-23 20:28:07 +0200

    Update test.py

--- test.py
+++ test.py
@@ -259,16 +259,11 @@ def investment():
 
             if stock_symbol:
                 st.write(f"Stock symbol for {company_name}: {stock_symbol}")
-
-                # Now, add the rest of your code for fetching stock data and performing calculations
-
-                # Example:
                 st.write("Fetching stock data...")
                 stock_data = get_stock_data(stock_symbol, start_date, end_date)
-
                 if stock_data is not None:
                     value = st.slider("If you were to invest:", min_value=100, max_value=5000, value=100, step=50)
-                    st.write(f"Perform calculations and display results here...")
+                    st.write(f"value is: {value}")
             else:
                 st.warning("Stock doesn't exist.")
 

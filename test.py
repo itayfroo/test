8c8929e53b4b1a005cfb13f56485076ffd3ac779
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-26 21:48:34 +0200

    d

--- test.py
+++ test.py
@@ -332,8 +332,39 @@ def investment():
                 st.warning(f"Stock doesn't exist.\ntry again or check your input.")
 
 
-from main import log_check 
-if log_check ==True:
+log_check =False
+st.title("Sign in page")
+
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
+
+if 'username' not in st.session_state:
+    st.session_state.username = ""
+
+if 'password' not in st.session_state:
+    st.session_state.password = ""
+
+def click_button():
+    st.session_state.clicked = True
+
+st.button('Sign up', on_click=click_button)
+
+if st.session_state.clicked:
+    st.text("Enter your credentials:")
+    st.session_state.username = st.text_input("Username:")
+    st.session_state.password = st.text_input("Password:", type="password")
+    
+    if st.button("Sign In"):
+        # Check credentials in the 'users.json' file
+        with open("users.json", "r") as file:
+            users = json.load(file)
+
+        if st.session_state.username in users and users[st.session_state.username] == st.session_state.password:
+            st.success("Logged in!")
+            log_check =True
+        else:
+            st.error("Check for username or password mistakes")
+if log_check == True:
     page = st.sidebar.radio("Select Page", ["Home", "Stock Analysis","real time stock investment"])
     if page == "Stock Analysis":
         stockanalyzer()

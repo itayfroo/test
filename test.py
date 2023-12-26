585f4c7990fb685a98fe839ce9d84e8a101f192c
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-26 21:55:36 +0200

    Update test.py

--- test.py
+++ test.py
@@ -336,24 +336,34 @@ log_check =False
 st.title("Sign in page")
 
 
-st.button('Sign up', on_click=click_button)
-
 if st.session_state.clicked:
-    st.text("Enter your credentials:")
-    st.session_state.username = st.text_input("Username:")
-    st.session_state.password = st.text_input("Password:", type="password")
-    
-    st.button('Sign-up', on_click=click_button)
-    if st.session_state.clicked:
-        with open("users.json", "r") as file:
-            users = json.load(file)
+    if 'sign_up_clicked' not in st.session_state:
+        st.text("Enter your credentials:")
+        st.session_state.username = st.text_input("Username:")
+        st.session_state.password = st.text_input("Password:", type="password")
+        
+        st.button('Sign-up', on_click=click_button)
+        if st.session_state.sign_up_clicked:
+            with open("users.json", "r") as file:
+                users = json.load(file)
 
-        st.button('Sign_up', on_click=click_button)
+            st.success("Account created! You can now log in.")
+    else:
+        st.text("Enter your credentials:")
+        st.session_state.username = st.text_input("Username:")
+        st.session_state.password = st.text_input("Password:", type="password")
+        
+        st.button('Log In', on_click=click_button)
         if st.session_state.clicked:
-            st.success("Logged in!")
-            log_check =True
-        else:
-            st.error("Check for username or password mistakes")
+            # Check credentials in the 'users.json' file
+            with open("users.json", "r") as file:
+                users = json.load(file)
+
+            if st.session_state.username in users and users[st.session_state.username] == st.session_state.password:
+                st.success("Logged in!")
+                log_check = True
+            else:
+                st.error("Check for username or password mistakes")
 
 if log_check == True:
     page = st.sidebar.radio("Select Page", ["Home", "Stock Analysis","real time stock investment"])

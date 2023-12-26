5e6902ada62f55bda1e7dcb5e0d762c44cce11ff
itayfroo <151866243+itayfroo@users.noreply.github.com>
2023-12-26 21:50:55 +0200

    Update test.py

--- test.py
+++ test.py
@@ -335,17 +335,6 @@ def investment():
 log_check =False
 st.title("Sign in page")
 
-if 'clicked' not in st.session_state:
-    st.session_state.clicked = False
-
-if 'username' not in st.session_state:
-    st.session_state.username = ""
-
-if 'password' not in st.session_state:
-    st.session_state.password = ""
-
-def click_button():
-    st.session_state.clicked = True
 
 st.button('Sign up', on_click=click_button)
 
@@ -354,16 +343,18 @@ if st.session_state.clicked:
     st.session_state.username = st.text_input("Username:")
     st.session_state.password = st.text_input("Password:", type="password")
     
-    if st.button("Sign In"):
-        # Check credentials in the 'users.json' file
+    st.button('Sign up', on_click=click_button)
+    if st.session_state.clicked:
         with open("users.json", "r") as file:
             users = json.load(file)
 
-        if st.session_state.username in users and users[st.session_state.username] == st.session_state.password:
+        st.button('Sign up', on_click=click_button)
+        if st.session_state.clicked:
             st.success("Logged in!")
             log_check =True
         else:
             st.error("Check for username or password mistakes")
+
 if log_check == True:
     page = st.sidebar.radio("Select Page", ["Home", "Stock Analysis","real time stock investment"])
     if page == "Stock Analysis":

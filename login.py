7c9842373bcd2b2241f7bb8f7900a8b4bbee58b7
itayfroo <151866243+itayfroo@users.noreply.github.com>
2024-01-28 21:30:19 +0200

    h

--- login.py
+++ login.py
@@ -2,7 +2,7 @@ import json
 import os
 import streamlit as st
 import pandas as pd
-
+import numpy as np
 json_file_path = "users.json"
 main_script_path = "test.py"
 
@@ -69,8 +69,21 @@ def sign_in(username, password):
             user_data = users.get(username)    
             if user_data and user_data.get("password") == password:
                 additional_info = users.get(f"{username}_info")
-                st.success(f"Welcome, {username}! Additional info: {additional_info}")
-                st.write(additional_info)
+                audio_file = open('myaudio.ogg', 'rb')
+                audio_bytes = audio_file.read()
+
+                st.audio(audio_bytes, format='audio/ogg')
+
+                sample_rate = 44100  # 44100 samples per second
+                seconds = 2  # Note duration of 2 seconds
+                frequency_la = 440  # Our played note will be 440 Hz
+                # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+                t = np.linspace(0, seconds, seconds * sample_rate, False)
+                # Generate a 440 Hz sine wave
+                note_la = np.sin(frequency_la * t * 2 * np.pi)
+
+                st.audio(note_la, sample_rate=sample_rate)
+                st.audio("")
                 d = {
                     'Username': username,
                     'Password': user_data,

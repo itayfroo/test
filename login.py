760a65b7d453650c8fabdad517475026e97c74c2
itayfroo <151866243+itayfroo@users.noreply.github.com>
2024-01-28 21:34:14 +0200

    Update login.py

--- login.py
+++ login.py
@@ -69,20 +69,7 @@ def sign_in(username, password):
             user_data = users.get(username)    
             if user_data and user_data.get("password") == password:
                 additional_info = users.get(f"{username}_info")
-                audio_file = open('myaudio.ogg', 'rb')
-                audio_bytes = audio_file.read()
-
-                st.audio(audio_bytes, format='audio/ogg')
-
-                sample_rate = 44100  # 44100 samples per second
-                seconds = 2  # Note duration of 2 seconds
-                frequency_la = 440  # Our played note will be 440 Hz
-                # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
-                t = np.linspace(0, seconds, seconds * sample_rate, False)
-                # Generate a 440 Hz sine wave
-                note_la = np.sin(frequency_la * t * 2 * np.pi)
-
-                st.audio(note_la)
+                
                
                 d = {
                     'Username': username,
